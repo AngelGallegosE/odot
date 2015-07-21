5d58ff884831c89783c8d881cef27cd32aefdf9d
AngelGallegos <7vonengel@gmail.com>
2015-07-20 22:51:01 -0500

    Add todo item model and associated specs

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ end
 
 group :test do
 	gem 'capybara'
+	gem 'shoulda-matchers'
 end
 
 
--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TodoList, type: :model do
+RSpec.describe TodoItem, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  it { should have_many(:todo_items) }
+  it { should belong_to (:todo_list) }
 end
